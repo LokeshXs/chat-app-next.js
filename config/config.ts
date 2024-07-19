867d1117d2a3507ed595a895d960f4712d87b541
Lokesh <77314004+LokeshXs@users.noreply.github.com>
2024-07-19 23:49:52 +0530

    changed the base url

--- config/config.ts
+++ config/config.ts
@@ -1,2 +1,3 @@
 export const WebSocketServerUrl = "ws://65.2.169.72:80"
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://chat-app-personal-project.ashwacreations.com/"
+// export const BASE_URL = "http://localhost:3000"
