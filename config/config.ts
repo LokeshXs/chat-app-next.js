ca73343da46a80dd378952c2f4bb68da3cad40d2
Lokesh <77314004+LokeshXs@users.noreply.github.com>
2024-07-20 17:34:33 +0530

    back to production config

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
-// export const WebSocketServerUrl = "wss://chat-app-websocket.ashwacreations.com"
-// export const BASE_URL = "https://chat-app-personal-project.ashwacreations.com/"
-export const WebSocketServerUrl = "ws://localhost:8080"
-export const BASE_URL = "http://localhost:3000"
+export const WebSocketServerUrl = "wss://chat-app-websocket.ashwacreations.com"
+export const BASE_URL = "https://chat-app-personal-project.ashwacreations.com/"
+// export const WebSocketServerUrl = "ws://localhost:8080"
+// export const BASE_URL = "http://localhost:3000"
